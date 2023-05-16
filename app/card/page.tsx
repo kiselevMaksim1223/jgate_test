ea06a658d622ea804573d6c4331354b575ed9583
Maksim_Kiselev <laurkurt@gmail.com>
2023-05-16 17:29:41 +0300

    main and card page styles

--- app/card/page.tsx
+++ app/card/page.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import {CardComponent} from '@/components/Card/card-component'
+import s from '../../styles/card-page.module.scss'
 
 export default function Card() {
   return (
-    <CardComponent/>
+    <section className={s.container}>
+      <CardComponent/>
+    </section>
   )
 }
